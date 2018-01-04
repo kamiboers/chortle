8a08a16c383c4b143eb2597fae915036fcb1cdd3
= <=>
2018-01-03 18:05:42 -0700

    partial templates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :password, presence: true
+  validates :username, presence: true, uniqueness: true,  length: { maximum: 20 }
+  validates :password, presence: true, length: { minimum: 8 }
   has_secure_password
 end
