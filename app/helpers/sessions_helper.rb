58218fc453d77a23df7b0ca14ace0a63d1141990
= <=>
2018-03-09 22:28:11 -0700

    authenticate user via cookie if present

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,16 @@
 module SessionsHelper
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
   
   def log_in(user)
     session[:user_id] = user.id
@@ -14,4 +26,5 @@ module SessionsHelper
     session[:user_id] = nil
     @current_user = nil
   end
+
 end
